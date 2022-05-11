9cb7c8ba406e39150964c108de21cd0abdf72581
sklv1 <82162346+sklv1@users.noreply.github.com>
2022-05-11 19:16:17 +0300

    безабит

---
+++ Poprigenok/Poprigenok/Windows/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _db = Poprigenok.Model.PoprigenokEntities;
+
+namespace Poprigenok.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DgAgentsSales.ItemsSource = _db.GetContext().Agents.ToList();
+        }
+
+        /// <summary>
+        /// Удаление агента
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnDeleteAgents_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (DgAgentsSales.SelectedCells.Count > 0)
+                {
+                    int id = DgAgentsSales.SelectedIndex;
+                    Model.Agents agents = _db.GetContext().Agents.Find(id);
+                    _db.GetContext().Agents.Remove(agents);
+                    _db.GetContext().SaveChanges();
+
+                    MessageBox.Show("Объект удален");
+                    DgAgentsSales.ItemsSource = _db.GetContext().Agents.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        /// <summary>
+        /// Переход на окно добавления агента
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnAddAgents_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.EditAgents editAgents = new Windows.EditAgents();
+            editAgents.ShowDialog();
+        }
+        /// <summary>
+        /// Переход на окно редактирования агента
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnEditAgents_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.EditAgents editAgents = new Windows.EditAgents();
+            editAgents.ShowDialog();
+        }
+    }
+}
---
+++ Poprigenok/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f6e68d4a-33c6-4421-bcb9-fd83cae45fb8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
